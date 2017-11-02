8d3b5bfadfa77698787e70238c2eaf8fe704e810
Panot Leelangamwongsa <panot.le@ku.th>
2017-11-03 03:46:33 +0700

    first commit

---
+++ desktop/src/com/panot/gdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.panot.gdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.panot.gdx.game.PacmanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PacmanGame(), config);
+	}
+}
